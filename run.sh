7dfa0468d7a4f0d86b6c6543055fe62194abe585
Vipul1020 <116952605+Vipul1020@users.noreply.github.com>
2022-10-29 17:21:14 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -70,3 +70,98 @@ if %activate%==/20 goto :disabletouchpad
 if %activate%==/21 goto :godmode
 if %activate%==/22 goto :dnslookup
 goto :mm
+:ipaddress
+color 07
+echo ------------------------------------------------------------------------------------------
+ipconfig
+echo ------------------------------------------------------------------------------------------
+pause
+goto :mm
+
+***********************************************************************************************
+
+:hostname
+color 05
+echo ------------------------------------------------------------------------------------------
+hostname
+echo ------------------------------------------------------------------------------------------
+pause
+goto :mm
+
+***********************************************************************************************
+
+:runningtasks
+color 04
+echo ------------------------------------------------------------------------------------------
+tasklist
+echo ------------------------------------------------------------------------------------------
+pause
+goto :mm
+
+***********************************************************************************************
+
+:portsandservices
+color 16
+echo ------------------------------------------------------------------------------------------
+echo Displays protocol statistics and current TCP/IP network connections.                     ]
+echo                                                                                          ]
+echo SERVICES:                                                                                ]
+echo a             Displays all connections and listening ports.                              ]
+echo                                                                                          ]   
+echo b             Displays the executable involved in creating each connection or            ]
+echo               listening port. In some cases well-known executables host                  ]
+echo               multiple independent components, and in these cases the                    ]
+echo               sequence of components involved in creating the connection                 ]
+echo               or listening port is displayed. In this case the executable                ]
+echo               name is in [] at the bottom, on top is the component it called,            ]
+echo               and so forth until TCP/IP was reached. Note that this option               ]
+echo               can be time-consuming and will fail unless you have sufficient             ]
+echo               permissions.                                                               ]
+echo                                                                                          ]
+echo e             Displays Ethernet statistics. This may be combined with the -s             ]
+echo               option.                                                                    ]
+echo                                                                                          ]
+echo f             Displays Fully Qualified Domain Names (FQDN) for foreign                   ]
+echo               addresses.                                                                 ]
+echo                                                                                          ]
+echo n             Displays addresses and port numbers in numerical form.                     ]
+echo                                                                                          ]
+echo o             Displays the owning process ID associated with each connection.            ]
+echo                                                                                          ]
+echo p proto       Shows connections for the protocol specified by proto; proto               ]
+echo               may be any of: TCP, UDP, TCPv6, or UDPv6.  If used with the -s             ]
+echo               option to display per-protocol statistics, proto may be any of:            ]
+echo               IP, IPv6, ICMP, ICMPv6, TCP, TCPv6, UDP, or UDPv6.                         ]
+echo                                                                                          ]
+echo q             Displays all connections, listening ports, and bound                       ]
+echo               nonlistening TCP ports. Bound nonlistening ports may or may not            ]
+echo               be associated with an active connection.                                   ]
+echo                                                                                          ]
+echo r             Displays the routing table.                                                ]
+echo                                                                                          ]
+echo s             Displays per-protocol statistics.  By default, statistics are              ]
+echo               shown for IP, IPv6, ICMP, ICMPv6, TCP, TCPv6, UDP, and UDPv6;              ]
+echo               the -p option may be used to specify a subset of the default.              ]
+echo                                                                                          ]
+echo t             Displays the current connection offload state.                             ]
+echo                                                                                          ]
+echo x             Displays NetworkDirect connections, listeners, and shared                  ]
+echo               endpoints.                                                                 ]
+echo                                                                                          ]
+echo y             Displays the TCP connection template for all connections.                  ]
+echo               Cannot be combined with the other options.                                 ]
+echo ------------------------------------------------------------------------------------------
+echo Which services do you want to access? 
+echo Type Service Name with hyphen like -a or -b only.
+echo [-a, -b, -e, -f, -n, -o, -p, -q, -r, -s, -t, -x, -y or interval]
+echo If any process needs Ctrl+c to end, type N in the confirmation to exit to main menu. Y will close whole program.
+pause
+set /p service= LazyAnt~
+netstat %service%
+echo ------------------------------------------------------------------------------------------
+echo DONE
+echo ------------------------------------------------------------------------------------------
+pause
+goto :mm
+
+***********************************************************************************************
